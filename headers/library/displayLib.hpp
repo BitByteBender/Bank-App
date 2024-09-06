adb904f1e0cea6609770d34c7521bffb220d3e1d
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-06 05:20:56 +0100

    Styling

--- headers/library/displayLib.hpp
+++ headers/library/displayLib.hpp
@@ -5,21 +5,52 @@
 
 using std::cout;
 using std::endl;
+using std::string;
 
 namespace displayLib
 {
   void MainMenuScreen(const char *Head)
   {
     cout<<"========================================\n"
-	<<"             "<<Head<<"                 \n"
+	<<"\t    "<<Head<<"\n"
 	<<"========================================\n";
-    cout<<"        [1]> Show Client List\n"
-	<<"        [2]> Add New Client\n"
-	<<"        [3]> Delete Client\n"
-	<<"        [4]> Update Client Info\n"
-	<<"        [5]> Find Client\n"
-	<<"        [6] Exit";
+    cout<<"\t[1]> Show Client List\n"
+	<<"\t[2]> Add New Client\n"
+	<<"\t[3]> Delete Client\n"
+	<<"\t[4]> Update Client Info\n"
+	<<"\t[5]> Find Client\n"
+	<<"\t[6]> Exit";
     cout<<"\n========================================\n"<<endl;
+  }
+
+  void Styler(string Border, uint16_t Len)
+  {
+    while (Len != 0) {
+      cout<<Border;
+      Len--;
+    }
+    cout<<'\n';
+  }
+  
+  void DisplayClientList(string TableStyling, uint16_t Len)
+  {
+    uint16_t i = 0;
+
+    Styler(TableStyling, Len);
+    
+    for (i = 0; i < 2 ; ++i) {
+      if (i == 0) {
+	cout<<" | Accout Number  "
+	    <<" | Pin Code  "
+	    <<" | Client Name\t\t"
+	    <<" | Phone\t\t"
+	    <<" | Balance  \n";
+      } else {
+	/* Records here */
+	cout<<"\t\t\t\t\tEmpty clients list\n"<<endl;
+      }
+      Styler(TableStyling, Len);
+    }
   }
 };
 
--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ using std::cout;
 using std::cin;
 using std::endl;
 using displayLib::MainMenuScreen;
+using displayLib::DisplayClientList;
 
 int main(void)
 {
   MainMenuScreen("Main Menu Screen");
+  cout<<'\n';
+  DisplayClientList("=====", 19);
+
   return (0);
 }
