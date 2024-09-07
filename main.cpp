7d03857bb914cb218eb75fd33f02dd40cc67e2ff
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-07 19:23:25 +0100

    Added a program loop and integrated the insert client, client list displayer and the main menu + exit functionalities

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
-//#include "headers/library/displayLib.hpp"
 #include "headers/core.hpp"
-/*
-using displayLib::MainMenuScreen;
-using displayLib::DisplayTable;
-using displayLib::DisplayRecords;
-*/
+
 
 int main(void)
 {
-  //vector <stClient> vClients = SaveRecords();
-  //vector <string> vRecs = SaveRecords(vClients);
-  //DisplayRecords(vClients);
-  //SaveRecordsToFile(vRecs);
-  //cout<<"Loading files:\n";
-  //cout<<LoadRecordsFromFile("Recs")[0]<<'\n';
-  //MainMenuScreen("Main Menu Screen");
-  cout<<'\n';
-  //DisplayTable("=====", 19);
-  vector <string> vRecs = LoadRecordsFromFile("Recs");
-  //vClients = LineToClientsRecord(vRecs);
-
-  DisplayClientsList(vRecs);
+  OnBeginPlay();
   return (0);
 }
